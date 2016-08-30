4edb579a3e1615fa79f1ca702c6edd0355dc8e30
ishdaddy7 <seanhan1@gmail.com>
2016-08-29 23:44:50 -0400

    adding some logging

--- browser/js/play/gameCanvas.js
+++ browser/js/play/gameCanvas.js
@@ -1,7 +1,7 @@
 app.directive('gameCanvas', function($window, $injector) {
 
   var linkFn = function(scope, ele, attrs) {
-
+    console.log('linkFn ran - line 4 of gameCanvas');
     scope.gameConfig = {
       width: 800,
       height: 600,
@@ -18,20 +18,12 @@ app.directive('gameCanvas', function($window, $injector) {
     RegexGame.game.state.add('Game', RegexGame.Game);
     RegexGame.game.state.start('Boot');
 
+    console.log('line 21 of gameCanvasjs', RegexGame.game.scope)
     var w = angular.element($window);
     w.bind('resize', function(evt) {
       // If the window is resized
     });
 
-
-//DOING THIS IS THE KEY TO PASSING SCOPE INTO THE GAME
-/*    mySocket.then(function(sock) {
-      require('./main.js')(
-        ele, scope, sock,
-        scope.ngModel,
-        scope.mapId,
-        $injector);
-    });*/
   };
 
   return {
